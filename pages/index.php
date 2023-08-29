329d4abb3cb13806c079fbb53005f0b5371429a9
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-29 15:15:40 +0100

    connected index page to database

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,10 @@
 <?php
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        header('location:login.php');
+    }
     include 'connect.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,8 +12,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
+
+    <h1>Welcome 
+        <?php
+            echo $_SESSION['username'];
+        ?>
+    </h1>
 </body>
 </html>
