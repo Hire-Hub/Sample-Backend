85f007bd557bcfcb2362c01f2a607580e0ad929c
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-30 14:41:47 +0100

    created an applicant/intern page

---
+++ pages/applicants.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e9f64432b6.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <!-- created navbar  -->
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid" id="container">
+          <a class="navbar-brand text-primary" id="navbar-brand" href="landing.php">JobHunt</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse px-5" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" id="text" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" id="text" aria-current="page" href="applicants.php">Applicants</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" id="text" aria-current="page" href="#">About</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+                <a href="logout.php" class="btn btn-outline-primary">Logout <i class="fa-solid fa-right-from-bracket"></i></a>
+             </form>
+          </div>
+        </div>
+      </nav>
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
