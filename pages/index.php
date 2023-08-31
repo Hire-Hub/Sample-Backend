a6e34f832b7cb3eab2028660d16ad73090b7e6af
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-31 10:52:18 +0100

    linked all pages

--- pages/index.php
+++ pages/index.php
@@ -13,7 +13,7 @@
     <!-- created navbar  -->
     <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid" id="container">
-          <a class="navbar-brand text-primary" id="navbar-brand" href="landing.php">JobHunt</a>
+          <a class="navbar-brand text-primary" id="navbar-brand" href="index.php">JobHunt</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
